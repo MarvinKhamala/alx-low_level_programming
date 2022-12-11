5b2d10feed5c8569e1becc9e66e136353e0fab9a
marvinkhamala <marvinkhamala96@gmail.com>
2022-12-11 11:20:50 -0800

    Updated command

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,7 +2,12 @@
 /**
  * main main block
  * Description: Print all combinations of two digits
- * Numbers printed in ascending order
+ * Numbers separated by comma
+ * Numbers separated by spaces
+ * 10 and 01 considered same combination of two different digits
+ * Only print smallest combination
+ * Numbers printed in ascending orde
+ * Only putchar is allowed
  * Return: 0
  */
 int main(void)
