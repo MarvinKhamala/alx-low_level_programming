6ccc57b2236919519958113013007806dec5ee9a
marvinkhamala <marvinkhamala96@gmail.com>
2022-12-11 10:32:23 -0800

    Task 5 initial commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		printf("%i", digit);
+	}
+	putchar('\n');
+	return (0);
+}
