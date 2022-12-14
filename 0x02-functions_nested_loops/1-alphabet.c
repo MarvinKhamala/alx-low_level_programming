8ded9c4b671fb25ce1a94b93cd9501b3c045ddc2
marvinkhamala <marvinkhamala96@gmail.com>
2022-12-14 11:48:26 -0800

    Task 1 initial commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet in lower case followed by a new line
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
