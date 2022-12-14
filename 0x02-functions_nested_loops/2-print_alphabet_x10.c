bc4c93d0e32a4758f9b5ceb721002da3c828a83d
marvinkhamala <marvinkhamala96@gmail.com>
2022-12-14 12:00:18 -0800

    task 2 initial commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10x lowercase alphabet followed by new line
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char ch;
+
+	for (i = 0, i <= 9; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
